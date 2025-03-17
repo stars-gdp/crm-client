5d89a92e4d26a2fbf8c6d2b2bdd7745aee630ef4
Aharon Zilberman <aharon.z@phytech.com>
2025-03-17 23:42:04 +0200

    fix

--- app/config/api.config.ts
+++ app/config/api.config.ts
@@ -1,5 +1,4 @@
-export const API_URL = "http://localhost:3000";
-// export const API_URL = "https://api.stars-gdp.com";
+export const API_URL = "https://api.stars-gdp.com";
 
 export const API_ENDPOINT = "/api";
 
